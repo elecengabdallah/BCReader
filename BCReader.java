fab2180fa03033989a6292afd48fea6c9da2a6f7
elecengabdallah <elecengabd_allah@yahoo.com>
2016-06-01 21:28:12 +0300

    Add files via upload

---
+++ BCReader.java
@@ -0,0 +1,216 @@
+/*
+ * This program developed by Abd-Allah Farag
+ * It is a simple program to communicate with Cognex DataMan 260 Barcode Reader
+ * Apache Telnet API is used here according to Apache license
+ * Developed in 31/05/2016 03:00 AM
+ * Version 0.1
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketException;
+import java.util.GregorianCalendar;
+
+import org.apache.commons.net.telnet.EchoOptionHandler;
+import org.apache.commons.net.telnet.InvalidTelnetOptionException;
+import org.apache.commons.net.telnet.SuppressGAOptionHandler;
+import org.apache.commons.net.telnet.TelnetClient;
+import org.apache.commons.net.telnet.TelnetNotificationHandler;
+import org.apache.commons.net.telnet.TerminalTypeOptionHandler;
+
+
+
+
+public class BCReader implements TelnetNotificationHandler {
+	
+	public FileWriter dataFileWrite;
+	
+	private InputStream in;
+	private OutputStream out;
+	private TelnetClient telnetSocket;
+	private BCReaderGUI gui = null;
+	private long time;
+	
+	private String serverAddress = null;
+	private int serverPort = 0;
+	
+	/**
+	 * Constructor
+	 */	
+	BCReader(String serverAddress, int serverPort, BCReaderGUI gui) {
+		this.serverAddress = serverAddress;
+		this.serverPort = serverPort;
+		this.gui = gui;
+		
+		try {
+			time = new GregorianCalendar().getTimeInMillis();
+			dataFileWrite = new FileWriter("dataFile" + String.valueOf(time) + ".txt");
+		} catch (IOException e1) {
+			gui.append("Error creating data file!!!");
+		}
+	}
+	
+	
+	@Override
+	public void receivedNegotiation(int negotioation_code, int option_code) {
+		String command = null;
+		switch (negotioation_code) {
+		case TelnetNotificationHandler.RECEIVED_DO:
+			command = "DO";
+			break;
+		case TelnetNotificationHandler.RECEIVED_DONT:
+			command = "DONT";
+			break;
+		case TelnetNotificationHandler.RECEIVED_WILL:
+			command = "WILL";
+			break;
+		case TelnetNotificationHandler.RECEIVED_WONT:
+			command = "WONT";
+			break;
+		case TelnetNotificationHandler.RECEIVED_COMMAND:
+			command = "COMMAND";
+			break;
+		default:
+			command = Integer.toString(negotioation_code);
+			break;
+		}
+		gui.append("Received " + command + " for option code " + option_code);
+	}
+	
+	/***
+	 * start the telnet communication
+	 * @return true if communication started
+	 */
+	public boolean start() {
+		// create telnet client socket object
+		telnetSocket = new TelnetClient();
+		
+//		set option handlers
+		TerminalTypeOptionHandler ttopt = new TerminalTypeOptionHandler("VT100", false, false, true, false);
+		EchoOptionHandler echoopt = new EchoOptionHandler(true, false, true, false);
+		SuppressGAOptionHandler gaopt = new SuppressGAOptionHandler(true, true, true, true);
+		
+//		try registering option handlers
+		try {
+			telnetSocket.addOptionHandler(ttopt);
+			telnetSocket.addOptionHandler(echoopt);
+			telnetSocket.addOptionHandler(gaopt);
+		} catch (InvalidTelnetOptionException e) {
+			gui.append("Error registering option handlers1: " + e.getMessage());
+		} catch (IOException e) {
+			gui.append("Error registering option handlers2: " + e.getMessage());
+		} 
+		
+//		try to connect Barcode Reader
+		try {
+			telnetSocket.connect(serverAddress, serverPort);
+			gui.append("Connected to Barcode Reader");
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			gui.append("Error connecting to Baracode Reader1: " + e.getMessage());
+			return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			gui.append("Error connecting to Baracode Reader2: " + e.getMessage());
+			return false;
+		}
+		
+//		try to get input and output streams
+		try {
+			in = telnetSocket.getInputStream();
+			out = telnetSocket.getOutputStream();
+			gui.append("Input/output streams Created");
+		} catch (Exception ioE) {
+			gui.append("Error creating input/output streams");
+			try {
+				telnetSocket.disconnect();
+			} catch (IOException e){}
+			return false;
+		}
+		
+		new ListenFromBCReader().start();
+
+		return true;
+	}
+	
+	/***
+	 * 
+	 * @param str Data to be recorded
+	 */
+	public void recordData(String str) {
+		try {
+			dataFileWrite.write(str);
+			dataFileWrite.flush();
+		} catch (Exception e) {
+			gui.append("Error recodrding data to data file");
+		}
+	}
+	
+	/***
+	 * send messages to Barcode Reader
+	 * @param msg input ... string to be sent.
+	 */
+	public void sendMsg(String msg) {
+		
+		byte[] outstr = new byte[1024];
+		int outstrLength = 0;
+		
+		outstr = msg.getBytes();
+		outstrLength = outstr.length;
+		
+		try {
+			out.write(outstr, 0, outstrLength);
+			out.flush();
+		} catch (Exception e) {
+			gui.append("Error sending msg to server!!!!");
+		}
+	}
+	
+	/***
+	 * disconnect method if error occurs or stop button pressed
+	 */
+	public void disconnect() {
+		
+		try {
+			telnetSocket.disconnect();
+			gui.append("Communication disconnected.");
+		} catch (Exception e) {
+			gui.append("Error disconnecting communication!!!");
+		}
+		
+	}
+	
+	// Listening thread gets data from Barcode Reader
+	class ListenFromBCReader extends Thread{
+
+		public void run() {
+			
+			byte[] inBuff = new byte[1024];
+			int inBuffLenght = 0;
+			String str = null;
+			
+			try {
+				do {
+					inBuffLenght = in.read(inBuff);
+					if(inBuffLenght > 0) {
+						str = new String(inBuff, 0, inBuffLenght);
+						gui.append(str);
+						recordData(str);
+					}
+				} while (inBuffLenght >= 0);
+			} catch (Exception e) {
+				gui.append("Error while reading from Barcode Reader: " + e.getMessage());
+			}
+			
+			try {
+				disconnect();
+			} catch(Exception e) {
+				gui.append("Error disconnecting from listening thread");
+			}
+		}
+	}
+
+}
+
---
+++ BCReaderGUI.java
@@ -0,0 +1,257 @@
+/*
+ * This program developed by Abd-Allah Farag
+ * It is a simple program to communicate with Cognex DataMan 260 Barcode Reader
+ * Apache Telnet API is used here according to Apache license
+ * Developed in 31/05/2016 03:00 AM
+ * Version 0.1
+ */
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+@SuppressWarnings("serial")
+public class BCReaderGUI extends JFrame implements ActionListener, WindowListener{
+	
+	private BCReader bcreader = null;
+	
+	private JLabel lPort, lAdd, lCmd;
+	private JTextField tfPort, tfAdd, tfCmd;
+	private JTextArea taResults;
+	private JButton btnStart, btnStop, btnSend;
+	
+	
+	
+	/**
+	 * Constructor
+	 * @param host
+	 * @param port
+	 */
+	BCReaderGUI() {
+//		set Frame title
+		super("BarCodeReader");
+		
+//		set Frame to Exit on close
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+//		add window listener to perform system exit
+		addWindowListener(this);
+		
+//		create connection data and start/stop communication panel 
+		JPanel connectionPanel = new JPanel(new GridLayout(2,3));
+		
+		lPort = new JLabel("Port:");
+		tfPort = new JTextField("23"); // use 23 as default port
+		
+		lAdd = new JLabel("Address:");
+		tfAdd = new JTextField("169.254.58.77"); // use 169.254.58.77 as default IP address
+		
+		btnStart = new JButton("Start");
+		btnStart.addActionListener(this);
+		
+		btnStop = new JButton("Stop");
+		btnStop.setEnabled(false);
+		btnStop.addActionListener(this);
+		
+		connectionPanel.add(lPort);
+		connectionPanel.add(tfPort);
+		connectionPanel.add(btnStart);
+		connectionPanel.add(lAdd);
+		connectionPanel.add(tfAdd);
+		connectionPanel.add(btnStop);
+		
+//		create Text area performs as console
+		JPanel resultsPanel = new JPanel();
+		
+		taResults = new JTextArea(20,80);
+		resultsPanel.add(new JScrollPane(taResults));
+		taResults.setEditable(true);
+		
+//		create commands text field panel
+		JPanel cmdPanel = new JPanel();
+		
+		lCmd = new JLabel("cmd");
+		tfCmd = new JTextField(60);
+		tfCmd.setEditable(false);
+		tfCmd.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int i = e.getKeyCode();
+				if(i == KeyEvent.VK_ENTER) {
+					bcreader.sendMsg(tfCmd.getText() + "\r\n");
+					tfCmd.setText("");
+				}
+				
+			}
+		});
+		
+		btnSend = new JButton("Send");
+		btnSend.setEnabled(false);
+		btnSend.addActionListener(this);
+		
+		cmdPanel.add(lCmd);
+		cmdPanel.add(tfCmd);
+		cmdPanel.add(btnSend);
+				
+//		add panels to main frame
+		this.add(connectionPanel, BorderLayout.NORTH);
+		this.add(resultsPanel, BorderLayout.CENTER);
+		this.add(cmdPanel, BorderLayout.SOUTH);
+		
+//		not resizable frame 
+		this.setResizable(false);
+		
+		this.pack();
+		this.setVisible(true);
+		
+	}
+	
+	/***
+	 * writes strings to the Text Area (console)
+	 * @param string input parameter ... the string to be written
+	 */
+	void append(String string) {
+		
+		taResults.append(string + "\n");
+	}
+	
+	/***
+	 * set buttons actions
+	 * 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		Object o = e.getSource();
+		
+//		Start button action
+		if(o == btnStart) {
+			String tempAdd = tfAdd.getText().trim(); // get address text field data
+			
+			if(tempAdd.length() == 0) {
+				append("Invalid IP Address");
+				return;
+			}
+				
+			String tempPort = tfPort.getText().trim(); // get port text field data
+			
+			if(tempPort.length() == 0) {
+				append("Invalid port no.!!!!");
+				return;
+			}
+			
+			int tempPortInt;
+			
+			try {
+				tempPortInt = Integer.parseInt(tempPort);
+			} catch (Exception intConversion) {
+				append("Invalid port no.!!!!");
+				return;
+			}
+			
+			append("Start communication...");
+			
+			bcreader = new BCReader(tempAdd, tempPortInt, this);
+			
+//			starts the BCReader object and check the start status
+			if(bcreader.start()) {
+				append("object created....");
+				btnStart.setEnabled(false);
+				tfAdd.setEditable(false);
+				tfPort.setEditable(false);
+				btnStop.setEnabled(true);
+				tfCmd.setEditable(true);
+				btnSend.setEnabled(true);
+			} else {
+				append("Failed to communicate!!!");
+			}	
+		}
+		
+//		Stop button action
+		if(o == btnStop) {
+			append("Disconneting communication...");
+			bcreader.disconnect();
+			updateGUI();
+		}
+		
+//		Send button action
+		if(o == btnSend) {
+			String cmdString = tfCmd.getText();
+			bcreader.sendMsg(cmdString + "\r\n");
+			tfCmd.setText("");
+		}
+		
+	}
+	
+	
+	/***
+	 * return the GUI to initial state
+	 */
+	public void updateGUI() {
+		
+		btnStart.setEnabled(true);
+		tfAdd.setEditable(true);
+		tfPort.setEditable(true);
+		btnStop.setEnabled(false);
+		tfCmd.setText("");
+		tfCmd.setEditable(false);
+		btnSend.setEnabled(false);
+	}
+	
+
+	/***
+	 * main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		new BCReaderGUI();
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+	@Override
+	public void windowClosed(WindowEvent e) {
+		bcreader.disconnect();
+		System.exit(0);
+	}
+	@Override
+	public void windowIconified(WindowEvent e) {}
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+	@Override
+	public void windowActivated(WindowEvent e) {}
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+	
+	/***
+	 * when window close button pressed exits the system
+	 */
+	@Override
+	public void windowClosing(WindowEvent e) {
+		
+	}
+}
